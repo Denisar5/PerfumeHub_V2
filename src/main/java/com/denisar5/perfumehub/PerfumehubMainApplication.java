0b93e9174fa53ff634cd33ef8fcc92a0f148e421
Denisar5 <denis.arnaudow@gmail.com>
2026-08-03 12:29:21 +0300

    chore: initialize Spring Boot application

---
+++ src/main/java/com/denisar5/perfumehub/PerfumehubMainApplication.java
@@ -0,0 +1,13 @@
+package com.denisar5.perfumehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumehubMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumehubMainApplication.class, args);
+	}
+
+}
